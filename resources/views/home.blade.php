8c66450c89c913235d594e71119bbf480dab44d5
RYANWILLIAMS <r007will@gmail.com>
2019-09-16 19:26:57 +0300

    Committted yaaasss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,10 +37,10 @@
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img class="d-block w-1000" src="images\guitar2.jpg" alt="First slide">
+                        <img class="d-block w-1000" src="" alt="First slide">
                         </div>
                         <div class="carousel-item">
-                        <img class="d-block w-1000" src="images\guitar1.jpg" alt="Second slide">
+                        <img class="d-block w-1000" src="" alt="Second slide">
                         </div>
                         <div class="carousel-item">
                         <img class="d-block w-1000" src="" alt="Third slide">
